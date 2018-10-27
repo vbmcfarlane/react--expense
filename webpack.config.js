e6ff10117b9df424e235ce2b53f0cca74d3055b2
vbmcfarlane <vbmcfarlane@gmail.com>
2018-10-27 15:27:25 -0600

    Adjustment to NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -5,9 +5,9 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
 if (process.env.NODE_ENV === 'test') {
-     require('dotenv').config({path: '.env.test '});
+     require('dotenv').config({path: '.env.test'});
 } else if (process.env.NODE_ENV === 'development') {
-          require('dotenv').config({path: '.env.development '});
+          require('dotenv').config({path: '.env.development'});
 }
 
 module.exports = (env) => {
@@ -49,7 +49,7 @@ module.exports = (env) => {
       CSSExtract,
       new webpack.DefinePlugin({
         'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTHDOMAIN': JSON.stringify(process.env.FIREBASE_AUTHDOMAIN),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
         'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
         'process.env.FIREBASE_PROJECT_ID ': JSON.stringify(process.env.FIREBASE_PROJECT_ID ),
         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),

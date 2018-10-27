38d2b680c3b67e0911e8acd3d2b396ea5f2d5c54
vbmcfarlane <vbmcfarlane@gmail.com>
2018-10-27 17:07:56 -0600

    DOTENV setup

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,23 +1,23 @@
 import * as firebase from 'firebase';
 // Firebase setup
 
-const config = {
-    apiKey: "AIzaSyAFyZ87tJF1m0588VAnOluD0CloCK-9iXE",
-    authDomain: "expensify-49e86.firebaseapp.com",
-    databaseURL: "https://expensify-49e86.firebaseio.com",
-    projectId: "expensify-49e86",
-    storageBucket: "expensify-49e86.appspot.com",
-    messagingSenderId: "934080961834"
-  };
+// const config = {
+//     apiKey: "AIzaSyAFyZ87tJF1m0588VAnOluD0CloCK-9iXE",
+//     authDomain: "expensify-49e86.firebaseapp.com",
+//     databaseURL: "https://expensify-49e86.firebaseio.com",
+//     projectId: "expensify-49e86",
+//     storageBucket: "expensify-49e86.appspot.com",
+//     messagingSenderId: "934080961834"
+//   };
 
-  // const config = {
-  //   apiKey: process.env.FIREBASE_API_KEY,
-  //   authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  //   databaseURL: process.env.FIREBASE_DATABASE_URl ,
-  //   projectId: process.env.FIREBASE_PROJECT_ID,
-  //   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-  // };
+  const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URl ,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  };
   
 
 // Initialize database

2e0e785d6c14b68803ab2dc526fb44c3d9a8e712
vbmcfarlane <vbmcfarlane@gmail.com>
2018-10-29 22:27:27 -0600

    Fix databaseURL

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,7 +13,7 @@ import * as firebase from 'firebase';
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URl ,
+    databaseURL: process.env.FIREBASE_DATABASE_URl,
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID

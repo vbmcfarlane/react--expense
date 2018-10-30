2de5225f92ee7be86a9534555e598a3101052b10
vbmcfarlane <vbmcfarlane@gmail.com>
2018-10-29 22:49:03 -0600

    arrrgh

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,14 +1,4 @@
 import * as firebase from 'firebase';
-// Firebase setup
-
-// const config = {
-//     apiKey: "AIzaSyAFyZ87tJF1m0588VAnOluD0CloCK-9iXE",
-//     authDomain: "expensify-49e86.firebaseapp.com",
-//     databaseURL: "https://expensify-49e86.firebaseio.com",
-//     projectId: "expensify-49e86",
-//     storageBucket: "expensify-49e86.appspot.com",
-//     messagingSenderId: "934080961834"
-//   };
 
 const config = {
     apiKey: process.env.FIREBASE_API_KEY,
@@ -20,8 +10,6 @@ const config = {
 };
   
 firebase.initializeApp(config);
-
-// Define database
 const database = firebase.database();
 
 export { firebase, database as default };

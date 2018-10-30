633eb77c5f2f9879ae2ce03b85012c354e3b068d
vbmcfarlane <vbmcfarlane@gmail.com>
2018-10-29 22:54:46 -0600

    once again

--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,5 +1,5 @@
-// require('dotenv').config({path:'.env.test'});
+require('dotenv').config({path:'.env.test'});
 
 // React 16
-import DotEnv from 'dotenv';
-DotEnv.config({ path: '.env.test' });
+// import DotEnv from 'dotenv';
+// DotEnv.config({ path: '.env.test' });
